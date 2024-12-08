4173e73b24cf03e524307a481dacf790768cb3d7
Dorian Scholz <112284719+Dorian-Scholz@users.noreply.github.com>
2024-12-07 19:38:40 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,25 @@
-function fib(n) {
-    return n;
+function fib(n)
+    {
+    
+    const fibArray = [];
+
+    
+    function computeFib(k) 
+    {
+        
+        if (k === 0) return 0;
+        if (k === 1) return 1;
+
+        if (fibArray[k] !== undefined) return fibArray[k];
+
+        fibArray[k] = computeFib(k - 1) + computeFib(k - 2);
+        return fibArray[k];
+    }
+
+    for (let i = 0; i <= n; i++)
+    {
+        fibArray[i] = computeFib(i);
+    }
+
+    return fibArray;
 }
