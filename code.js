116b5efc8e28ac9389597d1d966a8cca598ac1e2
Dorian Scholz <112284719+Dorian-Scholz@users.noreply.github.com>
2024-12-10 22:38:05 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,14 @@
-function fib(n) {
-    return n;
+function fib(n) 
+{
+    
+    const fib_solns = [0];
+    if (n >= 1) fib_solns[1] = 1;
+
+    
+    for (let i = 2; i <= n; i++)
+    {
+        fib_solns[i] = fib_solns[i - 1] + fib_solns[i - 2];
+    }
+
+    return fib_solns;
 }
